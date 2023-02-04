ef59a5969890b6d1f514bbaac276aa5213314d61
Vince Veselosky <vince@control-escape.com>
2023-02-04 08:33:32 -0500

    Factor out template test code

--- tests/test_templates.py
+++ tests/test_templates.py
@@ -5,20 +5,17 @@ from django.template import loader
 
 
 class TestTemplateSyntax(TestCase):
-    def test_template_syntax_html(self):
+    def _load_templates_for_glob(self, glob: str):
         project_dir = Path(__file__).parent.parent
         templates_dir = project_dir / "src" / "genericsite" / "templates"
-        for tpl_file in templates_dir.glob("**/*.html"):
+        for tpl_file in templates_dir.glob(glob):
             tpl_path = tpl_file.relative_to(templates_dir)
             with self.subTest(template=tpl_path):
                 # should NOT raise TemplateSyntaxError
                 template = loader.get_template(tpl_path)
 
+    def test_template_syntax_html(self):
+        return self._load_templates_for_glob("**/*.html")
+
     def test_template_syntax_txt(self):
-        project_dir = Path(__file__).parent.parent
-        templates_dir = project_dir / "src" / "genericsite" / "templates"
-        for tpl_file in templates_dir.glob("**/*.txt"):
-            tpl_path = tpl_file.relative_to(templates_dir)
-            with self.subTest(template=tpl_path):
-                # should NOT raise TemplateSyntaxError
-                template = loader.get_template(tpl_path)
+        return self._load_templates_for_glob("**/*.txt")
