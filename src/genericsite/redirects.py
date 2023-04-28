d3995f4f1aff1fbe373d4d19aebc397dacad2dd4
Vince Veselosky <vince@control-escape.com>
2023-04-27 20:37:31 -0400

    Implement temporary redirect middleware. Fixes #36

---
+++ src/genericsite/redirects.py
@@ -0,0 +1,18 @@
+from django.contrib.redirects.middleware import RedirectFallbackMiddleware
+from django.http import HttpResponseRedirect
+
+
+class TemporaryRedirectFallbackMiddleware(RedirectFallbackMiddleware):
+    """Replaces contrib.redirects RedirectFallbackMiddleware to send response code 302
+    (as recommended in the docs) rather than 301.
+    """
+
+    response_redirect_class = HttpResponseRedirect
+
+
+class PermanentRedirectFallbackMiddleware(RedirectFallbackMiddleware):
+    """For completeness, and empty subclass so users can always get their middleware
+    from genericsite.
+    """
+
+    pass
