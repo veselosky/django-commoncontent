a68932168b10bb91fcd1739f51103ecffc521a88
Vince Veselosky <vince@control-escape.com>
2023-05-06 07:29:01 -0400

    Add tests for correct filters on views

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,10 +1,12 @@
+from datetime import timedelta
+
 from django.contrib.auth.models import User
 from django.http import HttpResponseNotFound
 from django.test import TestCase
 from django.urls import reverse
 from django.utils import timezone
 
-from genericsite.models import Article, HomePage, Section, Site
+from genericsite.models import Article, HomePage, Page, Section, Site, Status
 
 
 class TestHomePageView(TestCase):
@@ -31,6 +33,134 @@ class TestHomePageView(TestCase):
         self.assertEqual(resp.status_code, 200)
         self.assertContains(resp, hp.title)
 
+    def test_homepage_site_filter(self):
+        """HP should always be for current site."""
+        site = Site.objects.get_current()
+        site2, _ = Site.objects.get_or_create(
+            id=2, defaults={"domain": "notmysite.com", "name": "notmysite.com"}
+        )
+        hp = HomePage.objects.create(
+            site=site,
+            admin_name="Test HomePage",
+            title="Test HomePage 1",
+            published_time=timezone.now() - timedelta(days=1),
+        )
+        hp2 = HomePage.objects.create(
+            site=site2,
+            admin_name="NOT MY HomePage",
+            title="NOT MY HomePage",
+            published_time=timezone.now(),
+        )
+        resp = self.client.get(reverse("home_page"))
+        self.assertEqual(resp.status_code, 200)
+        self.assertContains(resp, hp.title)
+
+    def test_draft_homepage_skipped(self):
+        "HPs not in published status should never be used by the view."
+        site = Site.objects.get_current()
+        hp = HomePage.objects.create(
+            site=site,
+            admin_name="Test HomePage",
+            title="Test HomePage 1",
+            published_time=timezone.now() - timedelta(days=1),
+        )
+        hp_draft = HomePage.objects.create(
+            site=site,
+            admin_name="DRAFT HomePage",
+            title="DRAFT HomePage 1",
+            published_time=timezone.now(),
+            status=Status.WITHHELD,
+        )
+        # Draft is newer and would be selected if not filtering on status.
+        resp = self.client.get(reverse("home_page"))
+        self.assertEqual(resp.status_code, 200)
+        self.assertContains(resp, hp.title)
+
+    def test_future_homepage_skipped(self):
+        "HPs with pub dates in future should never be used by the view."
+        site = Site.objects.get_current()
+        hp = HomePage.objects.create(
+            site=site,
+            admin_name="Test HomePage",
+            title="Test HomePage 1",
+            published_time=timezone.now() - timedelta(days=1),
+        )
+        hp_tomorrow = HomePage.objects.create(
+            site=site,
+            admin_name="FUTURE HomePage",
+            title="FUTURE HomePage 1",
+            published_time=timezone.now() + timedelta(days=1),
+        )
+        # HP scheduled for tomorrow should not be selected today
+        resp = self.client.get(reverse("home_page"))
+        self.assertEqual(resp.status_code, 200)
+        self.assertContains(resp, hp.title)
+
+
+class TestPageView(TestCase):
+    def test_page_not_found(self):
+        resp = self.client.get(
+            reverse("landing_page", kwargs={"page_slug": "page-not-created"})
+        )
+        self.assertIsInstance(resp, HttpResponseNotFound)
+
+    def test_draft_page(self):
+        site = Site.objects.get_current()
+        page = Page.objects.create(
+            site=site,
+            slug="test-page",
+            title="Test Page 1",
+            published_time=timezone.now(),
+            status=Status.WITHHELD,
+        )
+        resp = self.client.get(
+            reverse("landing_page", kwargs={"page_slug": "test-page"})
+        )
+        self.assertEqual(resp.status_code, 404)
+
+    def test_future_page(self):
+        site = Site.objects.get_current()
+        page = Page.objects.create(
+            site=site,
+            slug="test-page",
+            title="Test Page 1",
+            published_time=timezone.now() + timedelta(days=1),
+        )
+        resp = self.client.get(
+            reverse("landing_page", kwargs={"page_slug": "test-page"})
+        )
+        self.assertEqual(resp.status_code, 404)
+
+    def test_page_site_filter(self):
+        site = Site.objects.get_current()
+        site2, _ = Site.objects.get_or_create(
+            id=2, defaults={"domain": "notmysite.com", "name": "notmysite.com"}
+        )
+        page = Page.objects.create(
+            site=site2,
+            slug="test-page",
+            title="Test Page 1",
+            published_time=timezone.now(),
+        )
+        resp = self.client.get(
+            reverse("landing_page", kwargs={"page_slug": "test-page"})
+        )
+        self.assertEqual(resp.status_code, 404)
+
+    def test_page(self):
+        site = Site.objects.get_current()
+        page = Page.objects.create(
+            site=site,
+            slug="test-page",
+            title="Test Page 1",
+            published_time=timezone.now(),
+        )
+        resp = self.client.get(
+            reverse("landing_page", kwargs={"page_slug": "test-page"})
+        )
+        self.assertEqual(resp.status_code, 200)
+        self.assertContains(resp, page.title)
+
 
 class TestSectionView(TestCase):
     def test_empty_section(self):
@@ -56,12 +186,56 @@ class TestSectionView(TestCase):
         )
         self.assertIsInstance(resp, HttpResponseNotFound)
 
+    def test_draft_section(self):
+        site = Site.objects.get_current()
+        section = Section.objects.create(
+            site=site,
+            slug="test-section",
+            title="Test Section 1",
+            published_time=timezone.now(),
+            status=Status.WITHHELD,
+        )
+        resp = self.client.get(
+            reverse("section_page", kwargs={"section_slug": "test-section"})
+        )
+        self.assertEqual(resp.status_code, 404)
+
+    def test_future_section(self):
+        site = Site.objects.get_current()
+        section = Section.objects.create(
+            site=site,
+            slug="test-section",
+            title="Test Section 1",
+            published_time=timezone.now() + timedelta(days=1),
+        )
+        resp = self.client.get(
+            reverse("section_page", kwargs={"section_slug": "test-section"})
+        )
+        self.assertEqual(resp.status_code, 404)
+
+    def test_section_site_filter(self):
+        site = Site.objects.get_current()
+        site2, _ = Site.objects.get_or_create(
+            id=2, defaults={"domain": "notmysite.com", "name": "notmysite.com"}
+        )
+        section = Section.objects.create(
+            site=site2,
+            slug="test-section",
+            title="Test Section 1",
+            published_time=timezone.now(),
+        )
+        resp = self.client.get(
+            reverse("section_page", kwargs={"section_slug": "test-section"})
+        )
+        self.assertEqual(resp.status_code, 404)
+
 
 class TestArticlesAndFeeds(TestCase):
     @classmethod
     def setUpTestData(cls):
-        site, _ = Site.objects.get_or_create(
-            id=1, defaults={"domain": "example.com", "name": "example.com"}
+        site = Site.objects.get_current()
+        site2, _ = Site.objects.get_or_create(
+            id=2, defaults={"domain": "notmysite.com", "name": "notmysite.com"}
         )
         homepage = HomePage.objects.create(
             site=site,
@@ -75,20 +249,36 @@ class TestArticlesAndFeeds(TestCase):
             title="Test Section 1",
             published_time=timezone.now(),
         )
+        section2 = Section.objects.create(
+            site=site2,
+            slug="test-section",
+            title="Test Section 2",
+            published_time=timezone.now(),
+        )
         article = Article.objects.create(
             site=site,
             slug="test-article",
             section=section,
             title="Test Article 1",
+            published_time=timezone.now() - timedelta(days=1),
+        )
+        article2 = Article.objects.create(
+            site=site2,
+            slug="test-article",
+            section=section2,
+            title="Test Article 2",
             published_time=timezone.now(),
         )
         cls.site = site
+        cls.site2 = site2
         cls.homepage = homepage
         cls.section = section
+        cls.section2 = section2
         cls.article = article
+        cls.article2 = article2
 
     def test_article(self):
-        """Article page should contain metadata"""
+        """Article page should contain metadata."""
         resp = self.client.get(
             reverse(
                 "article_page",
@@ -96,8 +286,50 @@ class TestArticlesAndFeeds(TestCase):
             )
         )
         self.assertEqual(resp.status_code, 200)
+        # article2 from site2 has same slug, ensure we got the right one
         self.assertContains(resp, self.article.title)
         self.assertContains(resp, '''property="og:type" content="article"''')
+
+    def test_article_draft(self):
+        """Draft Article page should not be found."""
+        article = Article.objects.create(
+            site=self.site,
+            slug="draft-article",
+            section=self.section,
+            title="DRAFT Article 1",
+            published_time=timezone.now(),
+            status=Status.WITHHELD,
+        )
+        resp = self.client.get(
+            reverse(
+                "article_page",
+                kwargs={
+                    "section_slug": "test-section",
+                    "article_slug": "draft-article",
+                },
+            )
+        )
+        self.assertEqual(resp.status_code, 404)
+
+    def test_article_future(self):
+        """Future-dated Article page should not be found."""
+        article = Article.objects.create(
+            site=self.site,
+            slug="future-article",
+            section=self.section,
+            title="FUTURE Article 1",
+            published_time=timezone.now() + timedelta(days=1),
+        )
+        resp = self.client.get(
+            reverse(
+                "article_page",
+                kwargs={
+                    "section_slug": "test-section",
+                    "article_slug": "future-article",
+                },
+            )
+        )
+        self.assertEqual(resp.status_code, 404)
 
     def test_article_not_found(self):
         resp = self.client.get(
