00c314cebfe717002cf815b4c06400c28017be39
Vince Veselosky <vince@control-escape.com>
2024-03-27 07:28:54 -0400

    Fixes Separate the cacheable site content views from (necessarily) dynamic views #66

--- src/genericsite/urls.py
+++ src/genericsite/urls.py
@@ -13,19 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.urls import include, path
 from django.views.generic import RedirectView
 
-from genericsite import views as generic
+from genericsite import views as optional
 
 urlpatterns = [
-    path("tinymce/", include("tinymce.urls")),
-    path("accounts/profile/", generic.ProfileView.as_view(), name="account_profile"),
-    path(
-        "images/recent.json",
-        generic.TinyMCEImageListView.as_view(),
-        name="tinymce_image_list",
-    ),
     # Above pages are used for admin/publishing, not live site. These redirects are for
     # live site but cannot be statically generated. Users will have to deal with that.
     path(
@@ -34,22 +28,24 @@ urlpatterns = [
     path("feed/", RedirectView.as_view(pattern_name="site_feed")),
     # URLs below can be statically generated.
     # Home page pagination needs to come before the other page patterns to match.
-    path("page_<int:page>.html", generic.HomePageView.as_view(), name="home_paginated"),
+    path(
+        "page_<int:page>.html", optional.HomePageView.as_view(), name="home_paginated"
+    ),
     path(
         "<slug:section_slug>/page_<int:page>.html",
-        generic.SectionView.as_view(),
+        optional.SectionView.as_view(),
         name="section_paginated",
     ),
     path(
         "<slug:section_slug>/<slug:article_slug>.html",
-        generic.ArticleDetailView.as_view(),
+        optional.ArticleDetailView.as_view(),
         name="article_page",
     ),
     path(
-        "<slug:page_slug>.html", generic.PageDetailView.as_view(), name="landing_page"
+        "<slug:page_slug>.html", optional.PageDetailView.as_view(), name="landing_page"
     ),
-    path("<slug:section_slug>/", generic.SectionView.as_view(), name="section_page"),
-    path("<slug:section_slug>/index.rss", generic.SectionFeed(), name="section_feed"),
-    path("index.rss", generic.SiteFeed(), name="site_feed"),
-    path("", generic.HomePageView.as_view(), name="home_page"),
+    path("<slug:section_slug>/", optional.SectionView.as_view(), name="section_page"),
+    path("<slug:section_slug>/index.rss", optional.SectionFeed(), name="section_feed"),
+    path("index.rss", optional.SiteFeed(), name="site_feed"),
+    path("", optional.HomePageView.as_view(), name="home_page"),
 ]
